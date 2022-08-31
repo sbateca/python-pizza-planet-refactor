bcf0439e2187e858dc5bd6a3d56ecf6cf67f08b3
Sergio Bateca <sbtk87@hotmail.com>
2022-08-31 11:51:20 -0500

    Adjust variables names

--- app/common/utils.py
+++ app/common/utils.py
@@ -1,16 +1,13 @@
 import random
 import string
 
-min_value_randon_number = 1111111111
-max_value_randon_number = 9999999999
-
 
 def check_required_keys(keys: tuple, element: dict):
     return all(element.get(key) for key in keys)
 
 
-def get_random_number(min_value_randon_number, max_value_randon_number) -> int:
-    return random.randint(min_value_randon_number, max_value_randon_number)
+def get_random_number(min_value_random_number, max_value_random_number) -> int:
+    return random.randint(min_value_random_number, max_value_random_number)
 
 
 def get_random_price() -> str:
