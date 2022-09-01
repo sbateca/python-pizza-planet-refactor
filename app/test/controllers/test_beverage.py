05f1633ee5ceb266c5b8e72da1c12f73e03d121a
Sergio Bateca <sbtk87@hotmail.com>
2022-09-01 09:14:32 -0500

    Add test update function for beverage controller

--- app/test/controllers/test_beverage.py
+++ app/test/controllers/test_beverage.py
@@ -3,11 +3,28 @@ import pytest
 from app.controllers import BeverageController
 
 
-def test_create(app, beverage):
+def test_create(app, beverage: dict):
     created_beverage, error = BeverageController.create(beverage)
     pytest.assume(error is None)
     for param, value in beverage.items():
         pytest.assume(param in created_beverage)
         pytest.assume(value == created_beverage[param])
         pytest.assume(created_beverage['_id'])
-    
+
+
+def test_update(app, beverage: dict):
+    created_beverage, _ = BeverageController.create(beverage)
+    updated_fields = {
+        'name': 'updated',
+        'price': 10
+    }
+    updated_beverage, error = BeverageController.update({
+        '_id': created_beverage['_id'],
+        **updated_fields
+    })
+    pytest.assume(error is None)
+    beverage_from_database, error = BeverageController.get_by_id(created_beverage['_id'])
+    pytest.assume(error is None)
+    for param, value in updated_fields.items():
+        pytest.assume(updated_beverage[param] == value)
+        pytest.assume(beverage_from_database[param] == value)    
