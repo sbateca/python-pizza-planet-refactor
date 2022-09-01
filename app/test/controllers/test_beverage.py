2f0e11517296c649c055d282c0c0ffbb7bfa9d78
Sergio Bateca <sbtk87@hotmail.com>
2022-09-01 09:23:54 -0500

    Add test get all function for beverages controller

--- app/test/controllers/test_beverage.py
+++ app/test/controllers/test_beverage.py
@@ -27,4 +27,20 @@ def test_update(app, beverage: dict):
     pytest.assume(error is None)
     for param, value in updated_fields.items():
         pytest.assume(updated_beverage[param] == value)
-        pytest.assume(beverage_from_database[param] == value)    
+        pytest.assume(beverage_from_database[param] == value)
+
+
+def test_get_all(app, beverages: list):
+    created_beverages = []
+    for beverage in beverages:
+        created_beverage, _ = BeverageController.create(beverage)
+        created_beverages.append(created_beverage)
+
+    beverages_from_db, error = BeverageController.get_all()
+    searchable_beverages = {db_beverage['_id']: db_beverage for db_beverage in beverages_from_db}
+    pytest.assume(error is None)
+    for created_beverage in created_beverages:
+        current_id = created_beverage['_id']
+        assert current_id in searchable_beverages
+        for param, value in created_beverage.items():
+            pytest.assume(searchable_beverages[current_id][param] == value)
