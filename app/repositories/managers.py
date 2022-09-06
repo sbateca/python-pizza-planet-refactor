1ebcead34270f812e9c86f023bfbcebad2357440
Sergio Bateca <sbtk87@hotmail.com>
2022-09-06 10:01:01 -0500

    Remove unused library

--- app/repositories/managers.py
+++ app/repositories/managers.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional, Sequence
+from typing import Any, List, Optional, Sequence
 
 from sqlalchemy.sql import text, column
 from sqlalchemy import func, desc
