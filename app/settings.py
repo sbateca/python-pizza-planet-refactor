f0d081098a980d9ae867d0a6030552c19880127a
Sergio Bateca <sbtk87@hotmail.com>
2022-09-14 17:40:52 -0500

    Fix: adjust config class to manage env variables

--- app/settings.py
+++ app/settings.py
@@ -1,12 +1,10 @@
 import os
-from dotenv import load_dotenv
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-load_dotenv()
+
 
 class Config:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_DATABASE_URI = DATABASE_URI + os.path.join(BASE_DIR, 'pizza.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pizza.sqlite'))
